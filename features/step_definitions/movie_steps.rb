72fba4b0449c0733edebfba3884737e1619d2d21
Greg Bunia <greg.bunia@gmail.com>
2013-11-06 23:20:33 +0100

    Cucumber happy paths for sorting movies by title and by release date

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -15,7 +15,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  regexp = /#{Regexp.quote e1}.*#{Regexp.quote e2}/m
+  expect( page.body ).to match regexp
 end
 
 # Make it easier to express checking or unchecking several boxes at once
